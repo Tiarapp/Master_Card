774ab1bd4346d6eae4dbf6cf22c39957a0cfea70
p70090005 <p70090005@gmail.com>
2021-01-14 16:24:20 +0700

    14 Jan 2021

--- resources/views/admin/colorcombine/create.blade.php
+++ resources/views/admin/colorcombine/create.blade.php
@@ -10,18 +10,18 @@
             <div class="col-md-9">
                 <h4 class="modal-title">Tambah Color Combine</h4>
                 <hr>
-
+                
                 @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <strong>Error!</strong> 
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li></li>
-                            @endforeach
-                        </ul>
-                    </div>
+                <div class="alert alert-danger">
+                    <strong>Error!</strong> 
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li></li>
+                        @endforeach
+                    </ul>
+                </div>
                 @endif
-
+                
                 <form action="/admin/colorcombine/store" method="POST">
                     @csrf
                     <div class="row">
@@ -29,7 +29,7 @@
                             <div class="form-group">
                                 <label>Kode</label>
                                 <input type="text" class="form-control txt_line" name="kode" id="kode">
-    
+                                
                             </div>
                         </div>
                         <div class="col-md-12">
@@ -38,21 +38,21 @@
                                 <input type="text" class="form-control txt_line" name="nama" id="nama">
                             </div>
                         </div>
-                            <div class="col-md-12">
-                                <div class="row">
-                                <div class="col-md-3">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-2">
                                     <div class="form-group">
                                         {{-- <label>Warna 1</label> --}}
                                         <select class="js-example-basic-single col-md-12" name="idColor1" id="idColor1">
                                             <option value="" disabled selected>Warna 1</option>
                                             <option value="">Tidak Ada</option>
                                             @foreach ($color as $data)
-                                                <option value="{{ $data->id }}">{{ $data->nama }}</option>
+                                            <option value="{{ $data->id }}">{{ $data->nama }}</option>
                                             @endforeach
                                         </select>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-2">
                                     <div class="form-group">
                                         {{-- <label>Warna 2</label> --}}
                                         <select class="js-example-basic-single col-md-12" name="idColor2" id="idColor2">
@@ -64,7 +64,7 @@
                                         </select>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-2">
                                     <div class="form-group">
                                         {{-- <label>Warna 3</label> --}}
                                         <select class="js-example-basic-single col-md-12" name="idColor3" id="idColor3">
@@ -76,7 +76,7 @@
                                         </select>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-2">
                                     <div class="form-group">
                                         {{-- <label>Warna 4</label> --}}
                                         <select class="js-example-basic-single col-md-12" name="idColor4" id="idColor4">
@@ -88,20 +88,22 @@
                                         </select>
                                     </div>
                                 </div>
-                            </div>
-                            </div>
-                        <button type="submit" id="new" class="btn btn-primary" data-toggle="tooltip" data-placement="right" title="save">
-                            <i class='fas fa-plus-circle'></i>
-                        </button>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label>Branch</label>
-                                <input type="text" class="form-control txt_line" name="branch" id="branch">
+                                <div class="col-md-2">
+                                    <button type="submit" id="new" class="btn btn-primary" data-toggle="tooltip" data-placement="right" title="Add New Color">
+                                        <i class='fas fa-plus-circle fa-2x'></i>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                         <input type="hidden" class="form-control txt_line" name="createdBy" id="createdBy" value="{{ Auth::user()->name }}">
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        <div class="col-md-12">
+                            <button type="submit" id="save" class="btn btn-primary" data-toggle="tooltip" data-placement="right" title="save">
+                                <i class='far fa-check-square'></i>
+                            </button>
+                            <button type="button" id="cancel" class="btn" data-toggle="tooltip" data-placement="right" title="cancel">
+                                <a href="/admin/divisi">
+                                    <i class='far fa-window-close' style='color:red'></i>
+                                </a></button>
                         </div>
                     </div>
                 </form>
