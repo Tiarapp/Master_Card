daf42807483a6a53eaa6196dea6327a198e34a21
p70090005 <p70090005@gmail.com>
2021-06-28 08:20:47 +0700

    hasil produksi
    hasil produksi table, controller dan model masih kosong

---
+++ app/Http/Controllers/jenisDowntimeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\jenisDowntime;
+use Illuminate\Http\Request;
+
+class jenisDowntimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\jenisDowntime  $jenisDowntime
+     * @return \Illuminate\Http\Response
+     */
+    public function show(jenisDowntime $jenisDowntime)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\jenisDowntime  $jenisDowntime
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(jenisDowntime $jenisDowntime)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\jenisDowntime  $jenisDowntime
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, jenisDowntime $jenisDowntime)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\jenisDowntime  $jenisDowntime
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(jenisDowntime $jenisDowntime)
+    {
+        //
+    }
+}
