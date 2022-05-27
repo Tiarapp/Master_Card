51b201f0c1ed4a72e71681293f0836f19fb865d0
Tiarapp <tiar.adeperdanap@gmail.com>
2022-05-27 09:06:08 +0700

    koli & print mc

--- resources/views/admin/mastercard/create.blade.php
+++ resources/views/admin/mastercard/create.blade.php
@@ -551,6 +551,7 @@
                                                 <option value='20'>20 Koli</option>
                                                 <option value='25'>25 Koli</option>
                                                 <option value='50'>50 Koli</option>
+                                                <option value='00'>100 Koli</option>
                                             </select>
                                         </div>
                                         /Koli
