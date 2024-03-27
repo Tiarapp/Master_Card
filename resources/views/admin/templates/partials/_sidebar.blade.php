25175124965aea1cb2175c3ab2006e75232e55c9
Tiarapp <tiar.adeperdanap@gmail.com>
2024-03-27 13:48:30 +0700

    add menu qc

--- resources/views/admin/templates/partials/_sidebar.blade.php
+++ resources/views/admin/templates/partials/_sidebar.blade.php
@@ -359,6 +359,25 @@
               </ul>
             </li>
           @endif
+          @if (Auth::user()->divisi_id == 12 || Auth::user()->divisi_id == 2)
+            <li class="nav-item">
+              <a href="#" class="nav-link active">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                  QC
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="{{ route('qc.index') }}" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>COA</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          @endif
         </ul>
         
       </nav>
