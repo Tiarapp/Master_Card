314533946682117e96e935a242fadcd255ecfe5a
Tiar Ade Perdana Putra <tiarapp31@MacBook-Pro-Tiar.local>
2025-12-08 13:29:20 +0700

    fix edit realisasi

--- resources/views/admin/kontrak/edit_kirim.blade.php
+++ resources/views/admin/kontrak/edit_kirim.blade.php
@@ -1,11 +1,11 @@
-<div class="modal fade" id="edit_kirim{{ $o->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="edit_kirim{{ $data->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Edit Realisasi</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <form id="jquery-val-form" action="{{ route('kontrak.edit_kirim', $o->id) }}" method="post">
+      <form id="jquery-val-form" action="{{ route('kontrak.edit_kirim', $data->id) }}" method="post">
         {{csrf_field()}}
         {{ method_field('PUT') }}
         <div class="modal-body">
@@ -14,11 +14,11 @@
             
             <div class="mb-3">
               <label for="recipient-name" class="col-form-label">Tanggal Kirim:</label>
-              <input type="date" class="form-control" name="tglKirim" id="tglKirim" value="{{ $o->tanggal_kirim }}" required>
+              <input type="date" class="form-control" name="tglKirim" id="tglKirim" value="{{ $data->tanggal_kirim }}" required>
             </div>
             <div class="mb-3">
               <label for="message-text" class="col-form-label">Jumlah Kirim:</label>
-              <input type="number" class="form-control" name="jumlahKirim" id="jumlahKirim" value="{{ $o->qty_kirim }}" required>
+              <input type="number" class="form-control" name="jumlahKirim" id="jumlahKirim" value="{{ $data->qty_kirim }}" required>
             </div>
         </div>
         <div class="modal-footer">
