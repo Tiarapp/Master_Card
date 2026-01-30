06d8dd2a599d1fb2e920371a7407134471fe21c0
Tiar Ade Perdana Putra <tiarapp31@MacBook-Pro-Tiar.local>
2026-01-30 14:29:31 +0700

    change order export OPI

--- routes/web.php
+++ routes/web.php
@@ -438,7 +438,7 @@ Route::middleware(['auth'])->group(function (){
             });
         }
 
-        $opi = $opi->orderBy('id', 'desc')
+        $opi = $opi->orderBy('updated_at', 'desc')
             ->paginate(50, ['*'], 'page', $page);
         // dd($opi);
         return Excel::download(new OpiExport($opi), 'opi.xlsx');
