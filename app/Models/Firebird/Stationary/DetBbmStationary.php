f98cc222052a432e834821c48076c3464221af23
Tiarapp <tiar.adeperdanap@gmail.com>
2026-02-13 10:42:06 +0700

    add model from firebird

---
+++ app/Models/Firebird/Stationary/DetBbmStationary.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Firebird\Stationary;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetBbmStationary extends Model
+{
+    use HasFactory;
+}
