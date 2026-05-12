5c519be94453d6f9520ef3dea361f65aecb8f35a
Tiar Ade Perdana Putra <tiarapp31@MacBook-Pro-Tiar.local>
2026-05-12 11:54:23 +0700

    tampilan hanya update saja

--- routes/web.php
+++ routes/web.php
@@ -113,7 +113,7 @@ Route::get('/admin', function () {
             ->get();
     $jumlah_kontrak = count($kontrak);
 
-    $tracking_updates = \App\Models\Tracking::orderBy('created_at', 'desc')->take(50)->get();
+    $tracking_updates = \App\Models\Tracking::where('Event', 'LIKE', '%Update%')->orWhere('Event', 'LIKE', '%Ubah%')->orderBy('created_at', 'desc')->take(50)->get();
 
     return view('admin.index', compact('jumlah_kontrak','tonase','realisasi', 'all_periode','data', 'kontrak_open', 'tracking_updates'));
 })->middleware(['auth'])->name('admin');
