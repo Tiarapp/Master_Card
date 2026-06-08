d0aadb748f4732435cc82c0ad56ab1ae6b44b01a
Tiar Ade Perdana Putra <tiarapp31@MacBook-Pro-Tiar.local>
2026-06-08 15:33:30 +0700

    add logo fsc

--- resources/views/admin/opi/pdf.blade.php
+++ resources/views/admin/opi/pdf.blade.php
@@ -2286,7 +2286,8 @@
         <tr class="row0">
           <td class="column0">&nbsp;</td>
           <td class="column1 style70 s style44" colspan="9">PT. SARANA PACKAGING AGRAPANA</td>
-          <td class="column10 style2 null"><img src="{{ asset('images/tes.png') }}" alt="" width="50" height="50"></td>
+          {{-- <td class="column10 style2 null"><img src="{{ asset('images/tes.png') }}" alt="" width="50" height="50"></td> --}}
+          <td class="column10 style2 null"></td>
           <td class="column11 style3 null"></td>
         </tr>
         <tr class="row1">
