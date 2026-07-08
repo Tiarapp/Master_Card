eef5f2f4be73893527da981096c780a6e75dc4e4
Tiar Ade Perdana Putra <tiarapp31@MacBook-Pro-Tiar.local>
2026-07-08 17:06:47 +0700

    update store palet

--- resources/views/admin/sj_palet/create.blade.php
+++ resources/views/admin/sj_palet/create.blade.php
@@ -87,7 +87,7 @@
                 {{-- menampilkan error ketika ada field yang kosong, bersangkutan dengan controller --}}
                 @if ($errors->any())
                 <div class="alert alert-danger">
-                    <strong>Error!</strong> 
+                    <strong>Error!</strong>
                     <ul>
                         @foreach ($errors->all() as $error)
                         <li>{{ $errors }}</li>
@@ -140,7 +140,7 @@
                                         <select class='js-example-basic-single col-md-12' name="listCust" id="listCust" onchange="getCustomer()">
                                             <option value="PT. SUPRACOR SEJAHTERA">PT. SUPRACOR SEJAHTERA</option>
                                             @foreach ($customer as $data)
-                                            <option value="{{ $data->Nama }}|{{ $data->AlamatKirim }}">{{ $data->Nama }}</option>
+                                                <option value="{{ $data->Nama }}|{{ $data->AlamatKirim }}">{{ $data->Nama }}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -205,7 +205,7 @@
                             </div>
                         </div>
                     </div>
-                    
+
                     <table class="table table-bordered" id="">
                         <thead>
                             <tr>
@@ -219,8 +219,8 @@
                             <?php
                             $counts = 5;
                             // perulangan untuk membuat input detail
-                            for ($i=1; $i<=$counts; $i++) { 
-                                
+                            for ($i=1; $i<=$counts; $i++) {
+
                                 echo "<tr>";
                                     echo    "<td>";
                                         echo   "<select class='js-example-basic-single col-md-12' name='nama_$i' id='nama_$i' onchange='getData();'>";
@@ -239,21 +239,21 @@
                                             echo "<input type='hidden' name='nama[$i]' id='nama[$i]' readonly>";
                                         }
                                         ?>
-                                        
+
                                     </tbody>
                                 </table>
                                 <input type="hidden" class="form-control txt_line" name="createdBy" id="createdBy" value="{{ Auth::user()->name }}">
                                 <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                    <button type="submit" class="btn btn-primary simpan">Simpan</button>
                                 </div>
                             </form>
                         </div>
                     </div>
-                </div>    
+                </div>
             </div>
-            
+
             @endsection
-            
+
 @section('javascripts')
 <!-- Select2 JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
@@ -280,17 +280,23 @@
             document.getElementById("catatan1").value = "PALET PLASTIK UNTUK MENGANGKUT BOX SJ NO : ";
         }
     }
-    
+
     function getCustomer() {
         var data = document.getElementById('listCust').value;
-        
+
         var cust = data.split('|');
         var custNama = cust[0];
         var custAlamat = cust[1];
-        
+
         document.getElementById('namaCustomer').value = custNama;
         document.getElementById('alamatCustomer').value = custAlamat;
     }
+
+    $('.simpan').on('click', function(e) {
+        $(this).prop('disabled', true);
+        getCatatan();
+        $('form').submit();
+    });
 
     function getCatatan() {
         var catatan1 = document.getElementById("catatan1").value;
@@ -298,7 +304,7 @@
 
         document.getElementById("catatan").value = catatan1 + nosj;
     }
-    
+
     // fungsi untuk mengisi otomatis ke input
     function getData() {
         var data1 = document.getElementById("nama_1").value;
@@ -306,30 +312,30 @@
         var data3 = document.getElementById("nama_3").value;
         var data4 = document.getElementById("nama_4").value;
         var data5 = document.getElementById("nama_5").value;
-        
+
         if (data1 != '') {
             var arr1 = data1.split('|');
             var idpalet1 = arr1[0];
             var nama1 = arr1[1];
-            var ukuran1 = arr1[2]; 
+            var ukuran1 = arr1[2];
             document.getElementById("idpalet[1]").value = idpalet1;
             document.getElementById("nama[1]").value = nama1;
             document.getElementById("ukuran[1]").value = ukuran1;
-        } 
+        }
         if (data2 != '') {
             var arr2 = data2.split('|');
             var idpalet2 = arr2[0];
             var nama2 = arr2[1];
-            var ukuran2 = arr2[2]; 
+            var ukuran2 = arr2[2];
             document.getElementById("idpalet[2]").value = idpalet2;
             document.getElementById("nama[2]").value = nama2;
             document.getElementById("ukuran[2]").value = ukuran2;
-        } 
+        }
         if (data3 != '') {
             var arr3 = data3.split('|');
             var idpalet3 = arr3[0];
             var nama3 = arr3[1];
-            var ukuran3 = arr3[2]; 
+            var ukuran3 = arr3[2];
             document.getElementById("idpalet[3]").value = idpalet3;
             document.getElementById("nama[3]").value = nama3;
             document.getElementById("ukuran[3]").value = ukuran3;
@@ -338,16 +344,16 @@
             var arr4 = data4.split('|');
             var idpalet4 = arr4[0];
             var nama4 = arr4[1];
-            var ukuran4 = arr4[2]; 
+            var ukuran4 = arr4[2];
             document.getElementById("idpalet[4]").value = idpalet4;
             document.getElementById("nama[4]").value = nama4;
-            document.getElementById("ukuran[4]").value = ukuran4;                        
+            document.getElementById("ukuran[4]").value = ukuran4;
         }
         if (data5 != '') {
             var arr5 = data5.split('|');
             var idpalet5 = arr5[0];
             var nama5 = arr5[1];
-            var ukuran5 = arr5[2]; 
+            var ukuran5 = arr5[2];
             document.getElementById("idpalet[5]").value = idpalet5;
             document.getElementById("nama[5]").value = nama5;
             document.getElementById("ukuran[5]").value = ukuran5;
